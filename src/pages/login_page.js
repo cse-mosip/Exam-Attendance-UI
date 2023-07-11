d99fd80291ce2a304c2c56aef5b6ddb76528e161
thilinawaks1999 <thilinawks@gmail.com>
2023-07-11 22:40:13 +0530

    remove unwanted changes

--- src/pages/login_page.js
+++ src/pages/login_page.js
@@ -1,10 +1,5 @@
 import { Typography } from "@mui/material";
-import ExaminerCustomLoginComponent from "../components/ExaminerCustomLoginComponent/ExaminerCustomLoginComponent";
 
 export default function LoginPage() {
-  return (
-    <div>
-      <ExaminerCustomLoginComponent />
-    </div>
-  );
+  return <Typography>Login Page</Typography>;
 }
