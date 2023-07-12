d4367c50f8fcbc66d0abb46c5db6a179a836647f
Malith-19 <malith.19@cse.mrt.ac.lk>
2023-07-12 10:35:34 +0530

    Add dyanamic rendering.

--- src/pages/examiner-login-page.js
+++ src/pages/examiner-login-page.js
@@ -1,29 +1,37 @@
-import { Box, Typography } from "@mui/material"
-import ExaminerCustomLoginComponent from "../components/examiner-custom-login"
-import FingerprintScanner from "../components/fingerprint-scanner"
+import { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import ExaminerCustomLoginComponent from "../components/examiner-custom-login";
+import FingerprintScanner from "../components/fingerprint-scanner";
 
-const [isBioLogin, setIsBioLogin] = useState(false)
+export default function ExaminerLoginPage() {
+  const [isBioLogin, setIsBioLogin] = useState(false);
+  return (
+    <Box>
+      <Typography variant="h1" sx={styles.title}>
+        Examiner Login
+      </Typography>
 
-export default function ExaminerLoginPage(){
-    return (
-        <Box>
-            <Typography variant="h1" sx={styles.title}>Examiner Login</Typography>
-            
-            <FingerprintScanner isStudent={true}/>
+      {isBioLogin ? (
+        <FingerprintScanner isStudent={true} />
+      ) : (
+        <Box sx={styles.examinerLoginBio}>
+          <ExaminerCustomLoginComponent />
         </Box>
-    )
+      )}
+    </Box>
+  );
 }
 
 const styles = {
-    title:{
-        color:"#0170D6",
-        display:"flex",
-        justifyContent: "center",
-        padding:"4rem"
-    },
+  title: {
+    color: "#0170D6",
+    display: "flex",
+    justifyContent: "center",
+    padding: "4rem",
+  },
 
-    examinerLoginBio:{
-        display:"flex",
-        justifyContent: "center",
-    }
-}
+  examinerLoginBio: {
+    display: "flex",
+    justifyContent: "center",
+  },
+};
