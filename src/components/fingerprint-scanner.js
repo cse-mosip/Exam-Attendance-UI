9f828d828638b904dfe7248c516028feb0f264d9
springyboiii <sugumararvinth19991018@gmail.com>
2023-07-12 10:51:23 +0530

    Changed BG color of the image

--- src/components/fingerprint-scanner.js
+++ src/components/fingerprint-scanner.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Typography } from '@mui/material';
 import { Link } from "@mui/material";
 import { Box } from "@mui/material";
-import FingerprintImg from "../images/fp_2.jpg"
+import FingerprintImg from "../images/fp_3.png"
 import { Button } from "@mui/material";
 const  FingerprintScanner = ({isStudent, setIsBioLogin}) => {
   return (
 <Box sx={ headingStyle }>
     <Typography variant="h3">Please place your Finger on the Scanner</Typography>
-    <img src={FingerprintImg} alt="fingerprint image" style={{margin:"10%"}}/>
+    <img src={FingerprintImg} alt="fingerprint image" style={{margin:"10%"
+}}/>
     {isStudent && <Button variant="contained" color="error">STOP ATTENDING</Button>}
     <Button onClick={setIsBioLogin(false)} sx={{textDecoration:"underline", color:"grey"}} >Use {isStudent ? "Index":"Password"} instead</Button>
 
