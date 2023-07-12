69d4be6ddea3096ec3d2700f035adcf244b1f39a
sanduninduwara <sanduninduwara2@gmail.com>
2023-07-12 11:25:09 +0530

    add error page

--- src/pages/error-page.js
+++ src/pages/error-page.js
@@ -1,8 +1,81 @@
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Button, TextField, Link } from "@mui/material";
+import * as React from "react";
 
+export default function ErrorPage() {
+  
+  return (
+    <Box style={containerStyle}>
+     
+      <Box sx={formContainerStyle} component="form" Validate>
+      <Typography variant="h1" style={headingFormStyle404}>
+          404
+        </Typography>
+        <Typography variant="h3" style={headingFormStyle}>
+          Page Not Found
+        </Typography>
+        {/* <TextField
+          id="standard-basic"
+          label="Index Number:"
+          variant="standard"
+          size="small"
+          sx={textFieldStyle}
+          onChange={(e) => handleInputChange(e)}
+        /> */}
 
-export default function ErrorPage(){
-    return(
-        <Typography>Error Page</Typography>
-    );
+       
+        <Button size="lg" color="error" variant="contained" sx={stopButtonStyle}>
+          Home
+        </Button>
+
+      </Box>
+    </Box>
+  );
 }
+
+// Define style variables
+
+const containerStyle = {
+  marginTop: "10%",
+  textAlign: "center",
+};
+
+const formContainerStyle = {
+  borderRadius: 5,
+  width: "30%",
+  padding: "1%",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  margin: "0 auto",
+  backgroundColor: "#F6F6F6",
+  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+};
+
+const headingFormStyle = {
+  textAlign: "center",
+  marginTop: "5%",
+  color: "red",
+  letterSpacing: "1px",
+  fontSize: 32,
+  fontFamily: "Alata, sans-serif",
+  fontWeight: 450,
+};
+
+const headingFormStyle404 = {
+    textAlign: "center",
+    marginTop: "5%",
+    color: "red",
+    letterSpacing: "1px",
+    fontSize: 45,
+    fontFamily: "Alata, sans-serif",
+    fontWeight: 800,
+  };
+
+const stopButtonStyle = {
+  mt: 3,
+  mb: 2,
+};
+
