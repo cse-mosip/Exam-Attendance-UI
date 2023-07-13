0478801c533f8598c85dda7438e17c68df711b90
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-07-13 12:13:05 +0530

    Fix the suggested issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -7,8 +6,6 @@ import Button from '@mui/material/Button';
 
 
 function NavBar() {
-
-
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -16,11 +13,10 @@ function NavBar() {
             <IconButton color="inherit" aria-label="logo" sx={{marginLeft:'30px'}}>
             <img src="../LogoMain.png" alt="Logo" style={{ height: '160px' }} />
          </IconButton>
-          
           <Button variant="contained" color='error'>Log Out</Button>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
-export default NavBar
+export default NavBar;
