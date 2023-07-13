1fc1ab0c61671d4e1c6df771b09ca84f4e11d648
Jithmi Ranasinghe <79504341+Jithmi004@users.noreply.github.com>
2023-07-13 13:16:20 +0530

    Improvements to attendance monitoring page (#31)
    * Improvements to attendance monitoring page
    
    * confilcts resolved
    
    * Style changes
    
    * PR changes

--- src/components/attendance_summary.js
+++ src/components/attendance_summary.js
@@ -1,13 +1,17 @@
-import { Typography, Box } from '@mui/material';
+import { Box, Chip } from '@mui/material';
+import { Stack } from '@mui/system';
 
 const AttendanceSummary = (props) => {
     const totalCount = props.totalCount;
     const presentCount = props.presentCount;
     const percentage = totalCount > 0 ? presentCount / totalCount : "-";
+
     return (
         <Box>
-            <Typography>Total: {presentCount} / {totalCount}</Typography>
-            <Typography>Percentage: {percentage}</Typography>
+            <Stack direction="row" spacing={1}>
+                <Chip label={`Total: ${presentCount} / ${totalCount}`} />
+                <Chip label={`Percentage: ${percentage}`} />
+            </Stack>
         </Box>
     )
 }
--- src/pages/exam_attendance_report.js
+++ src/pages/exam_attendance_report.js
@@ -1,5 +1,6 @@
-import { Box, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Box, Typography, Button, Stack } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ExamAttendanceTable from '../components/exam_attendance_table';
 import AttendanceSummary from '../components/attendance_summary';
 
@@ -10,19 +11,19 @@ function createData(name, index, attendance, paperCollected) {
 const data = [
   createData('Kasun Kasun', "CS1456", "23.07.2023", ""),
   createData('Nimal Sirisena', "CS1456", "23.07.2023", ""),
-  createData('Dhammika Perera', "CS1456", "", "23.07.2023"),
+  createData('Pramila Perera', "CS1456", "", "23.07.2023"),
   createData('Kamal Induwara', "CS1456", "", "23.07.2023"),
   createData('Kasun Kasun', "CS1456", "23.07.2023", ""),
   createData('Nimal Sirisena', "CS1456", "23.07.2023", ""),
-  createData('Dhammika Perera', "CS1456", "", "23.07.2023"),
+  createData('Pramila Perera', "CS1456", "", "23.07.2023"),
   createData('Kamal Induwara', "CS1456", "", "23.07.2023"),
   createData('Kasun Kasun', "CS1456", "23.07.2023", ""),
   createData('Nimal Sirisena', "CS1456", "23.07.2023", ""),
-  createData('Dhammika Perera', "CS1456", "", "23.07.2023"),
+  createData('Pramila Perera', "CS1456", "", "23.07.2023"),
   createData('Kamal Induwara', "CS1456", "", "23.07.2023"),
   createData('Kasun Kasun', "CS1456", "23.07.2023", ""),
   createData('Nimal Sirisena', "CS1456", "23.07.2023", ""),
-  createData('Dhammika Perera', "CS1456", "", "23.07.2023"),
+  createData('Pramila Perera', "CS1456", "", "23.07.2023"),
   createData('Kamal Induwara', "CS1456", "", "23.07.2023"),
 ];
 
@@ -30,7 +31,7 @@ function getTotalCount() {
     return data.length;
 }
 
-function getPresentCount() {
+function getPresentCount() { 
     let result = 0;
     for (let i = 0; i < data.length; i++) {
         const row = data[i];
@@ -43,12 +44,38 @@ function getPresentCount() {
 
 const Report = () => {
   return (
-    <Box>
-        <Typography variant="h1">Attendance Monitoring</Typography>
-        <Link to="/">Back</Link> {/* need to change link to correct page */}
+    <Box sx={{ margin: '2rem', padding: '1rem' }}>
+      
+      <IconButton aria-label="delete">
+        <ArrowBackIosIcon />
+      </IconButton>
+      <Typography variant="h1" component="h1" align="center" gutterBottom color="#0170D6">
+        Attendance Monitoring
+      </Typography>  
+      <Box
+        display="flex"
+        justifyContent="right"
+        alignItems="right"
+      >
         <AttendanceSummary totalCount={getTotalCount()} presentCount={getPresentCount()}/>
-        <ExamAttendanceTable data={data}/>
-        <Link to="/start-attend">Start Attending</Link> {/* add link to attendance marking page */}
+      </Box>
+      <ExamAttendanceTable data={data}/>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Button
+          variant="contained"
+          sx={{
+            margin: "40px  0 20px 0",
+            width: "300px",
+            height: "40px",
+          }}
+        >
+          Start Attending
+        </Button>
+      </Box>
     </Box>
   );
 }
