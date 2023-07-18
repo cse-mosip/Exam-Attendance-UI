0596291f7e147d55c4dcb87d99bfd41594d4f1a9
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2023-07-18 16:11:37 +0530

    added navbar, exam date, aligned button with title

--- src/pages/exam_attendance_report.js
+++ src/pages/exam_attendance_report.js
@@ -1,9 +1,10 @@
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Grid } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ExamAttendanceTable from '../components/exam_attendance_table';
 import AttendanceSummary from '../components/attendance_summary';
 import { useNavigate } from 'react-router-dom';
+import NavBar from '../components/Navbar';
 
 function createData(name, index, attendance, paperCollected) {
   return { name, index, attendance, paperCollected };
@@ -46,42 +47,50 @@ function getPresentCount() {
 const Report = () => {
   const navigate = useNavigate()
   return (
-    <Box sx={{ margin: '2rem', padding: '1rem' }}>
-      
-      <IconButton aria-label="delete" onClick={()=>{navigate("/schedule")}}>
-        <ArrowBackIosIcon />
-      </IconButton>
-      <Typography variant="h1" component="h1" align="center" gutterBottom color="#0170D6">
-        Attendance Monitoring
-      </Typography>  
-      <Typography variant="h4" component="h1" align="center" gutterBottom color="#0170D6">
-      Professional Practice - CS1456
-      </Typography>  
+    <Box>
+      <NavBar/>
+      <Box sx={{ margin: '2rem', padding: '1rem' }}>
+        <Box display="flex" alignItems="center">
+          <Box>
+            <IconButton aria-label="delete" onClick={() => { navigate("/schedule") }}>
+              <ArrowBackIosIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ flexGrow: 1, textAlign: 'center' }}>
+            <Typography variant="h1" component="h1" gutterBottom color="#0170D6">
+              Attendance Monitoring
+            </Typography>
+            <Typography variant="h4" component="h1" align="center" gutterBottom color="#0170D6">
+              Professional Practice - CS1456 : 12/07/2023
+            </Typography>
+          </Box>
+        </Box>
 
-      <Box
-        display="flex"
-        justifyContent="right"
-        alignItems="right"
-      >
-        <AttendanceSummary totalCount={getTotalCount()} presentCount={getPresentCount()}/>
-      </Box>
-      <ExamAttendanceTable data={data}/>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Button
-          variant="contained"
-          sx={{
-            margin: "20px  0 10px 0",
-            width: "300px",
-            height: "40px",
-          }}
-          onClick = {()=>{navigate("/attendance-marking")}}
+        <Box
+          display="flex"
+          justifyContent="right"
+          alignItems="right"
         >
-          Start Attending
-        </Button>
+          <AttendanceSummary totalCount={getTotalCount()} presentCount={getPresentCount()}/>
+        </Box>
+        <ExamAttendanceTable data={data}/>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button
+            variant="contained"
+            sx={{
+              margin: "20px  0 10px 0",
+              width: "300px",
+              height: "40px",
+            }}
+            onClick = {()=>{navigate("/attendance-marking")}}
+          >
+            Start Attending
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
