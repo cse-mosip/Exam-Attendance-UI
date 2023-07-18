89192e107f7f5114f7716fe3a8f9d7b825bb5e5e
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2023-07-18 15:32:41 +0530

    Monitoring page table formatting

--- src/pages/exam_attendance_report.js
+++ src/pages/exam_attendance_report.js
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, Stack } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ExamAttendanceTable from '../components/exam_attendance_table';
@@ -10,22 +10,22 @@ function createData(name, index, attendance, paperCollected) {
 }
 
 const data = [
-  createData('Kasun Kasun', "190300A", "23.07.2023", ""),
-  createData('Nimal Sirisena', "190890P", "23.07.2023", ""),
-  createData('Pramila Perera', "190000X", "", "23.07.2023"),
-  createData('Kamal Induwara', "190215X", "", "23.07.2023"),
-  createData('Kasun Kasun', "190300A", "23.07.2023", ""),
-  createData('Nimal Sirisena', "190000X", "23.07.2023", ""),
-  createData('Pramila Perera', "190890P", "", "23.07.2023"),
-  createData('Kamal Induwara', "190300A", "", "23.07.2023"),
-  createData('Kasun Kasun', "190000X", "23.07.2023", ""),
-  createData('Nimal Sirisena', "190215X", "23.07.2023", ""),
-  createData('Pramila Perera', "190000X", "", "23.07.2023"),
-  createData('Kamal Induwara', "190890P", "", "23.07.2023"),
-  createData('Kasun Kasun', "190000X", "23.07.2023", ""),
-  createData('Nimal Sirisena', "190890P", "23.07.2023", ""),
-  createData('Pramila Perera', "190215X", "", "23.07.2023"),
-  createData('Kamal Induwara', "190890P", "", "23.07.2023"),
+  createData('Kasun Kasun', "190300A", "08:05 AM", "10.05 AM"),
+  createData('Nimal Sirisena', "190890P", "08:05 AM", ""),
+  createData('Pramila Perera', "190000X", "", "08:05 AM"),
+  createData('Kamal Induwara', "190215X", "", "08:05 AM"),
+  createData('Kasun Kasun', "190300A", "08:05 AM", ""),
+  createData('Nimal Sirisena', "190000X", "08:05 AM", ""),
+  createData('Pramila Perera', "190890P", "", "08:05 AM"),
+  createData('Kamal Induwara', "190300A", "", "08:05 AM"),
+  createData('Kasun Kasun', "190000X", "08:05 AM", ""),
+  createData('Nimal Sirisena', "190215X", "08:05 AM", ""),
+  createData('Pramila Perera', "190000X", "", "08:05 AM"),
+  createData('Kamal Induwara', "190890P", "", "08:05 AM"),
+  createData('Kasun Kasun', "190000X", "08:05 AM", ""),
+  createData('Nimal Sirisena', "190890P", "08:05 AM", ""),
+  createData('Pramila Perera', "190215X", "", "08:05 AM"),
+  createData('Kamal Induwara', "190890P", "", "08:05 AM"),
 ];
 
 function getTotalCount() {
@@ -74,7 +74,7 @@ const Report = () => {
         <Button
           variant="contained"
           sx={{
-            margin: "40px  0 20px 0",
+            margin: "20px  0 10px 0",
             width: "300px",
             height: "40px",
           }}
