bca08b44f153de9b719a4cf45bf6affcc7032614
Jithmi Ranasinghe <79504341+Jithmi004@users.noreply.github.com>
2023-07-18 16:25:53 +0530

    Update exam attendance report page (#54)
    * Monitoring page table formatting
    
    * added navbar, exam date, aligned button with title

--- src/components/exam_attendance_table.js
+++ src/components/exam_attendance_table.js
@@ -1,4 +1,5 @@
 import { useTheme } from '@mui/material/styles';
+import PropTypes from 'prop-types';
 import { useState } from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -15,75 +16,94 @@ import FirstPageIcon from '@mui/icons-material/FirstPage';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-const rowperpage = 5;
-
-const columns = [
-    { id: 'name', label: 'Name' },
-    { id: 'index', label: 'Index No' },
-    { id: 'attendance', label: 'Attendance' },
-    { id: 'collected', label: 'Paper Collected' },
-];
 
 function TablePaginationActions(props) {
     const theme = useTheme();
     const { count, page, rowsPerPage, onPageChange } = props;
-
-    const handleFirstPageButtonClick = (
-        event,
-    ) => {
-        onPageChange(event, 0);
+  
+    const handleFirstPageButtonClick = (event) => {
+      onPageChange(event, 0);
     };
-
+  
     const handleBackButtonClick = (event) => {
-        onPageChange(event, page - 1);
+      onPageChange(event, page - 1);
     };
-
+  
     const handleNextButtonClick = (event) => {
-        onPageChange(event, page + 1);
+      onPageChange(event, page + 1);
     };
-
+  
     const handleLastPageButtonClick = (event) => {
-        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
     };
-
+  
     return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </Box>
+      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="previous page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </Box>
     );
-}
+  }
+  
+  TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+  };
 
 const ExamAttendanceTable = (props) => {
     const rows = props.data;
     const [page, setpage] = useState(0);
-    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowperpage - rows.length) : 0;
+    
+
+
+
+    
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+//   const handleChangePage = (event, newPage) => {
+//     setPage(newPage);
+//   };
+
+//   const handleChangeRowsPerPage = (event) => {
+//     setRowsPerPage(parseInt(event.target.value, 10));
+//     setPage(0);
+//   };
+    function handleChangeRowsPerPage(event) {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setpage(0);
+    }
   
     function handleChangePage(event, newpage) {
         setpage(newpage);
@@ -91,29 +111,28 @@ const ExamAttendanceTable = (props) => {
 
     return (
         <Paper>
-            <TableContainer component={Paper} sx={{ marginTop: '4rem' }}>
-                <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <TableContainer component={Paper} sx={{ marginTop: '2rem', padding: 0}}>
+                <Table sx={{ minWidth: 500 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            {columns.map((column) => (
-                                <TableCell key={column.id}><Typography variant="h5" component="p">{column.label}</Typography></TableCell>
-                            ))}
+                            <TableCell ><Typography variant="h5" component="p">Name</Typography></TableCell>
+                            <TableCell align="center"><Typography variant="h5" component="p">Index Number</Typography></TableCell>
+                            <TableCell align="center"><Typography variant="h5" component="p">Attendance</Typography></TableCell>
+                            <TableCell align="right"><Typography variant="h5" component="p">Paper Collected</Typography></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {(rowperpage > 0
-                            ? rows.slice(page * rowperpage, page * rowperpage + rowperpage)
+                        {(rowsPerPage > 0
+                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                             : rows
-                        ).map((row, i) => {
-                            return (
-                                <TableRow key={i}>
-                                    <TableCell><Typography variant="body1" component="p">{row.name}</Typography></TableCell>
-                                    <TableCell><Typography variant="body1" component="p">{row.index}</Typography></TableCell>
-                                    <TableCell><Typography variant="body1" component="p">{row.attendance}</Typography></TableCell>
-                                    <TableCell><Typography variant="body1" component="p">{row.paperCollected}</Typography></TableCell>
-                                </TableRow>
-                            )
-                        })}
+                        ).map((row) => (
+                            <TableRow key={row.id} >
+                                <TableCell component="th" scope="row"><Typography variant="body1" component="p">{row.name}</Typography></TableCell>
+                                <TableCell align="center"><Typography variant="body1" component="p">{row.index}</Typography></TableCell>
+                                <TableCell align="center"><Typography variant="body1" component="p">{row.attendance}</Typography></TableCell>
+                                <TableCell align="right"><Typography variant="body1" component="p">{row.paperCollected}</Typography></TableCell>
+                            </TableRow>
+                        ))}
                         {emptyRows > 0 && (
                             <TableRow style={{ height: 47 * emptyRows }}>
                                 <TableCell colSpan={4} />
@@ -123,14 +142,21 @@ const ExamAttendanceTable = (props) => {
                     <TableFooter>
                         <TableRow>
                         <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            colSpan={4}
                             count={rows.length}
-                            rowsPerPage={rowperpage}
+                            rowsPerPage={rowsPerPage}
                             page={page}
+                            SelectProps={{
+                                inputProps: {
+                                'aria-label': 'rows per page',
+                                },
+                                native: true,
+                            }}
                             onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
                             ActionsComponent={TablePaginationActions}
-                            labelRowsPerPage=''
-                            rowsPerPageOptions={[]}
-                        />
+                            />
                         </TableRow>
                     </TableFooter>
                 </Table>
