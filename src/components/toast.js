44c1fd928e76a2bea380d95dbec200e6cda713c1
Prathu <prathushan99@gmail.com>
2023-07-19 15:42:56 +0530

    changed the toast position to top right

--- src/components/toast.js
+++ src/components/toast.js
@@ -1,13 +1,9 @@
 import { toast } from "react-toastify";
 
 export function successToast(message) {
-    toast.success(message,
-        { position: 'top-center' }
-    )
+    toast.success(message)
 }
 
 export function errorToast(message) {
-    toast.error(message,
-        { position: 'top-center' }
-    )
+    toast.error(message)
 }
