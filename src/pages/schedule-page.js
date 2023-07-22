736f388860983c91f6c43a7a7c7474aaf24c5c7a
yasirulakshan <yasiru1999@gmail.com>
2023-07-22 09:34:16 +0530

    Error Toast added

--- src/pages/schedule-page.js
+++ src/pages/schedule-page.js
@@ -1,52 +1,61 @@
-import { useEffect, useState } from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import { CircularProgress, Typography } from '@mui/material';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import { CircularProgress, Typography } from "@mui/material";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/appContext";
+import { errorToast } from "../components/toast";
 
 function TablePaginationActions(props) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
-    const handleBackButtonClick = (event) => {
-        onPageChange(event, page - 1);
-    };
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
 
-    const handleNextButtonClick = (event) => {
-        onPageChange(event, page + 1);
-    };
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
 
-    return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+    </Box>
+  );
 }
 
 const columns = [
@@ -76,14 +85,14 @@ export default function SchedulePage() {
     setPage(newPage);
   };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
-    const handleTableRowClick = (data) => {
-      navigate(`/exam-attendance-report/${data}`)
-    };
+  const handleTableRowClick = (data) => {
+    navigate(`/exam-attendance-report/${data}`);
+  };
 
   const fetchExamsSchedule = async () => {
     setIsLoadingExams(true);
@@ -99,7 +108,7 @@ export default function SchedulePage() {
       }
       setFetchExamsError({ isError: true, message: errorMessage });
       setIsLoadingExams(false);
-      //should call error toast after implementing
+      errorToast(errorMessage);
     }
   };
 
@@ -158,7 +167,7 @@ export default function SchedulePage() {
                     align="center"
                     sx={{ padding: "1rem" }}
                   >
-                    {fetchExamsError.message}
+                    Error while fetching exams, Refresh the page
                   </Typography>
                 </TableCell>
               </TableRow>
@@ -188,9 +197,11 @@ export default function SchedulePage() {
                 : exams
               ).map((row) => (
                 <TableRow
-                    hover
-                    key={row.id}
-                    onClick = {()=>{handleTableRowClick(row.id)}}
+                  hover
+                  key={row.id}
+                  onClick={() => {
+                    handleTableRowClick(row.id);
+                  }}
                 >
                   <TableCell>
                     <Typography variant="body1" component="p">
@@ -241,17 +252,17 @@ export default function SchedulePage() {
           )}
           <TableFooter>
             <TableRow>
-                <TablePagination
-                    colSpan={3}
-                    count={exams.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    ActionsComponent={TablePaginationActions}
-                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                    labelRowsPerPage={<Typography>Rows per Page: </Typography>}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
+              <TablePagination
+                colSpan={3}
+                count={exams.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                ActionsComponent={TablePaginationActions}
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                labelRowsPerPage={<Typography>Rows per Page: </Typography>}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
             </TableRow>
           </TableFooter>
         </Table>
